5602f9e324efbac68f3e5160bf590d396be3df00
Rob Meijer <pibara@gmail.com>
2020-09-07 17:38:07 +0200

    some docstrings

--- aioflureedb/__init__.py
+++ aioflureedb/__init__.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# pylint: disable=too-few-public-methods
+# pylint: disable=too-many-arguments
 """Basic asynchonous client library for FlureeDB"""
 import asyncio
 import json
@@ -7,14 +9,47 @@ from aioflureedb.signing import DbSigner
 class FlureeException(Exception):
     """Base exception class for aioflureedb"""
     def __init__(self,*args,**kwargs):
+        """Constructor
+
+        Parameters
+        ----------
+        args : list
+               List of positional arguments for passing to base class.
+        kwargs : dict
+                 Dictionary with named arguments for passing to base class.
+        """
         Exception.__init__(self,*args,**kwargs)
 
 class _FlureeQlQuery:
     """Helper class for FlureeQL query syntactic sugar"""
-    def __init__(self, endpoint, method):
+    def __init__(self, endpoint):
+        """Constructor
+
+        Parameters
+        ----------
+        endpoint : _FlureeQlEndpoint
+                   API endpoint for communicating FlureeQL queries with FlureeDB
+        """
         self.endpoint = endpoint
         self.permittedkeys = set(["select","selectOne","selectDistinct","from", "where","block","prefixes","vars","opts"])
     async def __call__(self, **kwargs):
+        """FlureeQl query construction through keyword arguments
+
+        Parameters
+        ----------
+        kwargs: dict
+                Keyword arguments for different parts of a FlureeQL query.
+
+        Raises
+        ------
+        TypeError
+            If an unknown kwarg value is used.
+
+        Returns
+        -------
+        dict
+            json decode result from the server.
+        """
         obj = dict()
         for key, value in kwargs.items():
             if key == "ffrom":
@@ -24,14 +59,47 @@ class _FlureeQlQuery:
             obj[key] = value
         return await self.endpoint.actual_query(obj)
     async def raw(self, obj):
+        """Use a readily constructed FlureeQL dictionary object to invoke the query API endpoint.
+
+        Parameters
+        ----------
+        obj : Complete FlureeQl query object.
+
+        Returns
+        -------
+        dict
+            json decode result from the server.
+        """
         return await self.endpoint.actual_query(obj)
+
 
 class _UnsignedGetter:
     """Get info with a GET instead of a POST"""
     def __init__(self, session, url):
+        """Constructor
+
+        Parameters
+        ----------
+        session : aiohttp.ClientSession
+                  HTTP session for doing HTTP post/get with
+        url : string
+              URL of the API endpoint.
+        """
         self.session = session
         self.url = url
     async def __call__(self):
+        """Invoke the functor
+
+        Returns
+        -------
+        dict
+            JSON decoded response from the server
+
+        Raises
+        ------
+        FlureeException
+            If the server returns something different than a 200 OK status
+        """
         if self.session:
             async with self.session.get(self.url) as resp:
                 if resp.status != 200:
@@ -42,11 +110,28 @@ class _UnsignedGetter:
             print("############# GET ##############")
             print("url:", self.url)
             print("################################")
-            return {"dryrun": true}
+            return {"dryrun": True}
 
 class _SignedPoster:
     """Basic signed HTTP posting"""
     def __init__(self, session, signer, url, required, optional, unsigned):
+        """Constructor
+
+        Parameters
+        ----------
+        session : aiohttp.ClientSession
+            HTTP session for doing HTTP post/get with
+        signer : aioflureedb.signing.DbSigner
+            ECDSA signer for Fluree transactions and queries
+        url : string
+            URL of the API endpoint
+        required : set
+            Set of required fields for the specific API call.
+        optional : set
+            Set of optional fields for the specific API call.
+        unsigned : bool
+            If True, don't sign posts.
+        """
         self.session = session
         self.signer = signer
         self.url = url
@@ -67,6 +152,11 @@ class _SignedPoster:
         -------
         string
             Content as returned by HTTP server
+
+        Raises
+        ------
+        FlureeException
+            When Fluree server returns a status code other than 200
         """
         if self.session:
             async with self.session.post(self.url, data=body, headers=headers) as resp:
@@ -85,8 +175,25 @@ class _SignedPoster:
             print("############ BODY ##############")
             print(body)
             print("################################")
-            return '{"dryrun": true}'
+            return {"dryrun": True}
     async def __call__(self, **kwargs):
+        """Invoke post API
+
+        Parameters
+        ----------
+        kwargs : dict
+                 Keyword arguments for the POST API call.
+
+        Returns
+        -------
+        dict
+            JSON decoded response from FlureeDB server
+
+        Raises
+        ------
+        TypeError
+            If an unknown kwarg is used on invocation OR a required kwarg is not supplied
+        """
         kwset = set()
         kwdict = dict()
         for key, value in kwargs.items():
@@ -109,21 +216,74 @@ class _SignedPoster:
 class _Network:
     """Helper class for square bracket interface to Fluree Client"""
     def __init__(self, flureeclient, netname, options):
+        """Constructor
+
+        Parameters
+        ----------
+        flureeclient : FlureeClient
+                       FlureeClient object to use as reference.
+        netname : string
+                  Name of the network for net/db fluree database naming.
+        options : set
+                  Set with existing databases within network.
+        """
         self.client = flureeclient
         self.netname = netname
         self.options = options
     def __getitem__(self, key):
+        """Square brackets operator
+
+        Parameters
+        ----------
+        key : string
+              Name of the desired database
+
+        Returns
+        -------
+        _DbFunctor
+            Function for constructing a Fluree Database client.
+
+        Raises
+        ------
+        KeyError
+            When a non defined database is requested.
+        """
         database = self.netname + "/" + key
         if not key in self.options:
             raise KeyError("No such database: '" + database + "'")
         return _DbFunctor(self.client, database)
 
 class _DbFunctor:
-    """Other helper class for square bracket interface to Fluree Client"""
+    """Helper functor class for square bracket interface to Fluree Client"""
     def __init__(self, client, database):
+        """Constructor
+
+        Parameters
+        ----------
+        client : FlureeClient
+                 FlureeClient object to use as reference.
+        database : string
+                   Full database name
+        """
         self.client = client
         self.database=database
     def __call__(self, privkey, auth_address, sig_validity=120, sig_fuel=1000):
+        """Invoke functor
+
+        Parameters
+        ----------
+        privkey : string
+                  Private key for the specific DB.
+        auth_address : atring
+                  Auth ID belonging with the privkey
+        sig_validity : int
+                       Validity in seconda of signatures.
+        sig_fuel : int
+                   Not sure what this is for, consult FlureeDB documentation for info.
+        Returns:
+         _FlureeDbClient
+            FlureeClient derived client for a specific DB
+        """
         return _FlureeDbClient(privkey,
                               auth_address,
                               self.database,
@@ -193,6 +353,13 @@ class FlureeClient:
         -------
         object
             Endpoint object suitable for API endpoint.
+
+        Raises
+        ------
+        AttributeError
+            When a non-defined fluree endpoint is designated
+        NotImplementedError
+            When a fluree API endpoint is designated that hasn't been implemented yet.
         """
         if api_endpoint not in self.known_endpoints:
             raise AttributeError("FlureeDB has no endpoint named " + api_endpoint)
@@ -227,6 +394,23 @@ class FlureeClient:
             return _UnsignedGetter(self.session, url)
         return _SignedPoster(self.session, self.signer, url, required, optional, unsigned=True)
     async def __getitem__(self, key):
+        """Square bracket operator
+
+        Parameters
+        ----------
+        key : string
+              Network name, should be defined on server.
+
+        Raises
+        ------
+        KeyError
+            When a non-defined network is designated.
+
+        Returns
+        -------
+        _Network
+            Helper object for designating databases within a network.
+        """
         databases = await self.dbs()
         options = set()
         for pair in databases:
@@ -243,10 +427,8 @@ class FlureeClient:
 
 
 
-# pylint: disable=too-few-public-methods
 class _FlureeDbClient:
     """Basic asynchonous client for FlureeDB representing a particular database on FlureeDB"""
-    # pylint: disable=too-many-arguments
     def __init__(self,
                  privkey,
                  auth_address,
@@ -364,7 +546,7 @@ class _FlureeDbClient:
                     print("############ BODY ##############")
                     print(body)
                     print("################################")
-                    return '{"dryrun": true}'
+                    return {"dryrun": True}
 
             async def header_signed(self, query_body):
                 """Do a HTTP query using headers for signing
@@ -400,7 +582,6 @@ class _FlureeDbClient:
                 headers = {"content-type": "application/json"}
                 return await self._post_body_with_headers(body, headers)
 
-        # pylint: disable=too-few-public-methods
         class FlureeQlEndpoint:
             """Endpoint for JSON based (FlureeQl) queries"""
             def __init__(self, api_endpoint, client):
@@ -416,7 +597,7 @@ class _FlureeDbClient:
                 self.stringendpoint = _StringEndpoint(api_endpoint, client)
             def __getattr__(self, method):
                 if api_endpoint == "query":
-                    return _FlureeQlQuery(self, method)
+                    return _FlureeQlQuery(self)
                 else:
                     raise AttributeError("FlureeQlEndpoint has no attribute named " + method)
             async def actual_query(self, query_object):
