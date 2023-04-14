12db21947bc8671e2ca6792e987acb1497f8fb77
Rob Meijer <rob@noemail>
2023-04-14 13:00:35 +0200

    list to single value

--- aioflureedb/__init__.py
+++ aioflureedb/__init__.py
@@ -436,9 +436,7 @@ class _SignedPoster:
             if "options" not in kwdict:
                 kwdict["options"] = {}
             if "root" not in kwdict["options"]:
-                kwdict["options"]["root"] = []
-            if self.signer.auth_id not in kwdict["options"]["root"]:
-                kwdict["options"]["root"].append(self.signer.auth_id)
+                kwdict["options"]["root"] = self.signer.auth_id
         body = json.dumps(kwdict, indent=4, sort_keys=True)
         headers = {"Content-Type": "application/json"}
         if not self.unsigned:
